a2aabddf278970cc8ec8ea219335129ba42fd80b
vimla kushwaha <vimlakushwaha01@gmail.com>
2024-05-11 23:31:24 +0530

    Initial Commit

---
+++ xcode/xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeApp.swift
+//  xcode
+//
+//  Created by vimla kushwaha on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
